c306ef2bd588875635876618397c150e31d78849
Chuka <emenike.chuka@gmail>
2022-12-19 13:18:30 -0800

    my commit low level

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,34 @@
-#include<stdio.h>
+#include "holberton.h"
+
+
 
 /**
-*main - just print
-*Description: print puthcar
-*Return: always zero
-*/
+ *
+ * main - prints Holberton
+ *
+ * Description: prints Holberton
+ *
+ * Return: 0 (Success)
+ *
+ */
 
 int main(void)
-
+	
 {
-	printf("_putchar\n");
+	char text[10] = "Holberton";
+
+	int i = 0;
+
+	for (i = 0; i < 9; i++)
+
+	{
+	
+			_putchar(text[i]);
+	
+		}
+
+	_putchar('\n');
+
 	return (0);
-}
+
+}}
