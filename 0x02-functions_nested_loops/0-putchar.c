3f73d3b1e46825eb6d08c214a69f5556eecf250b
Chuka <emenike.chuka@gmail>
2022-12-19 14:27:14 -0800

    my commit low level

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,17 +11,14 @@
  */
 
 int main(void)
-	
 {
-	char text[10] = "Holberton";
+	char text[9] = "Anything";
 	int i = 0;
-
 	for (i = 0; i < 9; i++)
 	{
-        	_putchar(text[i]);
+		_putchar(text[i]);
 	}
 	_putchar('\n');
-
 	return (0);
 
 }
