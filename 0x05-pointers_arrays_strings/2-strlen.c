3314403550bdcc5b6a6bafba108a987dcec22f64
Chuka <emenike.chuka@gmail>
2022-12-20 07:45:22 -0800

    my PSA commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -9,16 +9,12 @@
   */
 
 
-	int _strlen(char *s)
+int _strlen(char *s)
 	{
-
-		int len = 0;
-
-		while (s[len] != '\0')
-					{
-						len++;
-
-					}
-				return (len);
-
+	int len = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
 	}
