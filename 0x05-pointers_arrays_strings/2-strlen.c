a24fc386828d67ea961a6145b552c18474124b03
Chuka <emenike.chuka@gmail>
2022-12-20 07:48:40 -0800

    my PSA commit

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 
-
 /**
  * _strlen - returns length of string
  * @s: string
@@ -12,10 +11,11 @@
 int _strlen(char *s)
 	{
 	int len = 0;
-	
+
 	while (s[len] != '\0')
 	{
 		len++;
 	}
+
 	return (len);
 	}
