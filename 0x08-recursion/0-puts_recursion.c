2cfd05be8ea08fa68521da874d8bb0bb8372717d
Chuka <emenike.chuka@gmail>
2023-01-04 13:40:57 -0800

    Add function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,16 +7,15 @@
  */
 
 void _puts_recursion(char *s)
-
 {
 	if (*s != '\0')
 		{
 			_putchar(*(s + 0));
 			_puts_recursion((s + 1));
 		}
-			else
-			{
-				_putchar('\n');
+		else
+		{
+			_putchar('\n');
 			return;
-			}
+		}
 }
